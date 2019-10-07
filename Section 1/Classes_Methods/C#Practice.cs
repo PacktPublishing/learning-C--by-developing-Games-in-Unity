61bf0782f736b5bdffc8e3c6bdcd7d103481087c
cleonb-packt <35484449+cleonb-packt@users.noreply.github.com>
2019-10-07 11:23:28 +0530

    Add files via upload

---
+++ Section 1/Classes_Methods/C#Practice.cs
@@ -0,0 +1,22 @@
+//all code is contained in a class
+//all code to be run in the application will be containted in
+     //between the curly backets of the program 
+//We can name our program anything we want
+//all C# files end with the .cs extension which describes the type of file it is
+class OurProgram 
+{
+	//this is a method a program normally have many methods in them
+	//methods can use other methods which is called calling a method 
+	//when a method is called it can run code inside that method 
+	//void means the method does not pass or return any information. The type of 
+	  //information it returns is called the return type and is often defined next
+	  //to the method name . 
+	//A method can take a "parameter" which is basically the type of information
+	   //it is looking for to be passed into the method 
+	//all methods to be run first must be named Main
+	static void Main()
+	{
+
+
+	}
+}
